d70841630bef7e695d08d154d2b07391d31a0a96
sogore <sogoreibrahim135@gmail.com>
2023-10-24 01:14:42 +0000

    squelle du dashbord

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContainerComponent } from './components/container/container.component';
+import { Error404Component } from './components/error404/error404.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { EntityComponent } from './components/forms/entity/entity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    ContainerComponent,
+    Error404Component,
+    ModalComponent,
+    PaginationComponent,
+    LoadingComponent,
+    EntityComponent
   ],
   imports: [
     BrowserModule,
